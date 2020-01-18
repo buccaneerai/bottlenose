accc8691494a8439e4148fb659723fc9103b6e00
Brian <brianflaherty64@gmail.com>
2020-01-18 13:09:15 -0500

    feature: added predict operator to rxsgd

--- packages/rxsgd/src/operators/predict.js
+++ packages/rxsgd/src/operators/predict.js
@@ -5,10 +5,15 @@ import predictValue from '../internals/predictValue';
 const predict = function predict({
   intercept,
   weights,
-  featuresTrainedOn,
+  // featuresTrainedOn,
 }) {
   return source$ => source$.pipe(
-    map(row => predictValue({row, intercept, weights}))
+    map(row => predictValue({row, intercept, weights})),
+    // map(predictedValue => (
+    //   confidence > 0.5
+    //   ? {label: 1, confidence}
+    //   : {label: 0, confidence}
+    // ))
   );
 };
 
