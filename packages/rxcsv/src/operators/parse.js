b8560e8009b825d80a126494447491af7bbd1219
Brian <brianflaherty64@gmail.com>
2020-01-20 13:00:10 -0500

    rxcsv: updates to parse operator

--- packages/rxcsv/src/operators/parse.js
+++ packages/rxcsv/src/operators/parse.js
@@ -1,8 +1,24 @@
-import {scan} from 'rxjs/operators';
+import csvParse from 'csv-parse/lib/sync';
+import {filter,map,scan,tap} from 'rxjs/operators';
 
-const parse = function parse() {
+const parse = function parse(options = {}) {
   return csvStr$ => csvStr$.pipe(
-    // scan(csvPart => , '')
+    scan(([, lastChunk], fileChunk) => (
+      `${lastChunk}${fileChunk}`.split('\n')
+    ), ['', '']),
+    // tap(console.log),
+    scan(([,index, keys], [row]) => [
+      row,
+      index + 1,
+      (
+        index === -1
+        ? row.split(',')
+        : keys
+      )
+    ], [null, -1, null]),
+    filter(([,index]) => index > 0),
+    tap(console.log),
+    map(([csvRow, index, keys]) => csvParse(csvRow, {columns: keys, ...options}))
   );
 };
 
