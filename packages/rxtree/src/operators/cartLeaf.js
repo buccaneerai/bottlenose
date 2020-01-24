04540e17ddc426c27fab3743ad84b82b74798c16
Brian <brianflaherty64@gmail.com>
2020-01-24 10:10:58 -0500

    test: cartLeaf now has reasonable unit tests

--- packages/rxtree/src/operators/cartLeaf.js
+++ packages/rxtree/src/operators/cartLeaf.js
@@ -3,140 +3,43 @@ import flow from 'lodash/fp/flow';
 import times from 'lodash/fp/times';
 import randomstring from 'randomstring';
 import {of,zip} from 'rxjs';
-import {map, mergeMap, scan, takeLast} from 'rxjs/operators';
+import {map, mergeMap, scan, takeLast, tap} from 'rxjs/operators';
 
 import giniGain from './giniGain';
 import bestSplit from './bestSplit';
-
-// function getLabelCountForSamples({
-//   columns$,
-//   sampleId$,
-// }) {
-//   const sampleIdSub$ = sampleId$.pipe(share());
-//   const labelCounter$ = sampleIdSub$.pipe(
-//     toArray(),
-//     mergeMap(sampleIds => zip(
-//       columns$.pipe(
-//         mergeMap(cols => cols[0]),
-//         count() // FIXME - this could be delegated to
-//       ),
-//       of()
-//     )),
-//     map(([positiveCount, totalCount]) => ({
-//       0: totalCount - positiveCount,
-//       1: positiveCount,
-//     })),
-//   );
-//   return labelCounter$;
-// }
-
-// function getLeafIndex({
-//   middleware$,
-//   sampleId$,
-//   leafId
-// }) {
-//   const { mapCols } = middleware$;
-//   const index$ = sampleId$.pipe(
-//     toArray(),
-//     mergeMap(sampleIds => middleware$.pipe(
-//       mapCols(
-//         row => (sampleIds.indexOf(row._id) === -1 ? 0 : 1),
-//         {colName: leafId}
-//       )
-//     ))
-//   );
-//   return index$;
-// }
-
-// function getBestSplits({
-//   columnStreams,
-//   totalLabelCount,
-//   gainOperator,
-//   maxConcurrentColumnStreams,
-//   _bestSplit = bestSplit
-// }) {
-//   const split$ = of(...columnStreams).pipe(
-//     scan((memo, columnSampleAscending$) => ([
-//       columnSampleAscending$,
-//       memo[1] + 1
-//     ]), [null, -1]),
-//     mergeMap(([columnSampleAscending$, columnIndex]) => combineLatest([
-//       columnSampleAscending$.pipe(_bestSplit(totalLabelCount, gainOperator)),
-//       of(columnIndex)
-//     ]), maxConcurrentColumnStreams), // FIXME - this will drop extra columns...
-//     map(([bSplit, columnIndex]) => ({...bSplit, splitColumn: columnIndex}))
-//   );
-//   return split$;
-// }
 
 const makeLeaf = function makeLeaf({
   leafId,
   numSamples,
   depth,
   parentId,
+  childIds = null,
 }) {
-  return split$ => split$.pipe(
-    scan((bestSplitVal, split) => (
-      !bestSplitVal || split.gain > bestSplitVal.gain
-      ? split
-      : bestSplitVal
-    ), null),
-    takeLast(1),
+  // Accepts an Observable where each item is an array, containing all of the
+  // potential splits in the form of {splitValue: <Number>, gain: <Number>}
+  return splits$ => splits$.pipe(
+    tap(console.log),
+    map(splits => (
+      splits.reduce((bSplit, split, index) => (
+        !bSplit || split.gain > bSplit.gain
+        ? {...split, columnIndex: index}
+        : bSplit
+      ), null)
+    )),
+    tap(console.log),
     map(bestSplitVal => ({
       parentId,
       numSamples,
       depth,
       _id: leafId,
       splitValue: bestSplitVal.splitValue,
-      splitColumn: bestSplitVal.splitColumn,
+      splitColumn: bestSplitVal.columnIndex,
       splitGain: bestSplitVal.gain,
-      childIds: flow(times(() => randomstring.generate(17)))(2),
+      splitLabelCounts: bestSplitVal.labelCounts,
+      childIds: childIds || flow(times(() => randomstring.generate(17)))(2),
     })),
   );
 };
-
-// const oldcartLeaf = function oldcartLeaf({
-//   leafId,
-//   numSamples, // count of the samples in the column
-//   columnIndexes,
-//   parentId,
-//   depth,
-//   gainOperator = giniGain,
-//   _getSamplesInColumn = getSamplesInColumn,
-//   _getBestSplits = getBestSplits,
-//   _makeLeaf = makeLeaf,
-//   _getLabelCountForSamples = getLabelCountForSamples,
-//   _getLeafIndex = getLeafIndex,
-//   maxConcurrentColumnStreams = 3,
-// }) {
-//   const sampleIdSub$ = sampleId$.pipe(share());
-//   const totalLabelCountSub$ = _getLabelCountForSamples({
-//     sampleIds: sampleIdSub,
-//   }).pipe(shareReplay(1));
-//   const columnStreams = columnIndexes.map(i => _getSamplesInColumn({
-//     middleware$,
-//     sampleId$: sampleIdSub$,
-//     totalLabelCount$: totalLabelCountSub$,
-//     columnIndex: i
-//   }));
-//   const index$ = _getLeafIndex({
-//     middleware$,
-//     leafId,
-//     sampleId$: sampleIdSub$,
-//   });
-//   const split$ = _getBestSplits({
-//     maxConcurrentColumnStreams,
-//     columnStreams,
-//     gainOperator,
-//     totalLabelCount, // FIXME
-//   });
-//   const leaf$ = zip([split$, index$]).pipe(
-//     takeLast(1),
-//     map(([split]) => split),
-//     _makeLeaf({leafId, depth, parentId, numSamples}),
-//   );
-//   return leaf$;
-// };
 
 const cartLeaf = function cartLeaf({
   depth,
@@ -144,22 +47,25 @@ const cartLeaf = function cartLeaf({
   numSamples,
   parentId,
   totalLabelCounts,
+  childIds = null,
   gainOperator = giniGain,
   _bestSplit = bestSplit,
   _makeLeaf = makeLeaf,
 }) {
   // columns$$ is an Observable of arrays.  Each item in the array is an
-  // observable containing the samples for this leaf in each column in the form
-  // of {index, feature, label}
+  // observable containing the column values for this leaf in the form
+  // of {index, feature, label}, pre-sorted in ascending order on the feature value.
+  // The values should also be pre-filtered to only include samples that belong in
+  // this leaf.
   return sortedColumns$$ => sortedColumns$$.pipe(
     mergeMap(columns$ => zip(
       ...columns$.map(col$ => (
         col$.pipe(_bestSplit(totalLabelCounts, gainOperator))
       ))
     )),
-    map(splits$ => of(splits$)),
+    // mergeMap(splits => of(...splits.map((s, index) => ))),
     takeLast(1),
-    _makeLeaf({leafId, depth, parentId, numSamples})
+    _makeLeaf({leafId, depth, parentId, numSamples, childIds})
   );
 };
 
--- packages/rxtree/src/operators/cartLeaf.test.js
+++ packages/rxtree/src/operators/cartLeaf.test.js
@@ -6,13 +6,7 @@ import {from, of} from 'rxjs';
 import {map,mapTo} from 'rxjs/operators';
 
 import cartLeaf, { testExports } from './cartLeaf';
-const {
-  getBestSplits,
-  getSamplesInColumn,
-  getLabelCountForSamples,
-  getLeafIndex,
-  makeLeaf
-} = testExports;
+const {makeLeaf} = testExports;
 
 const fakeLeaf = {
   parentId: 'foogranny',
@@ -26,101 +20,46 @@ const fakeLeaf = {
 };
 
 describe('cartLeaf operator', () => {
-  // it('should return accurate leaf when given valid data', done => {
-  //   const onData = sinon.spy();
-  //   const onError = sinon.spy();
-  //   const sampleIds = [0, 1, 2, 3, 4];
-  //   const middleware = {};
-  //   const bestSplit = {gain: 0.5, splitValue: 5, splitColumn: 2};
-  //   const leaf = {...fakeLeaf};
-  //   const params = {
-  //     sampleId$: from(sampleIds),
-  //     middleware$: {
-  //       ...of(middleware),
-  //       pipe: sinon.stub().returns(),
-  //       count: sinon.stub().returns(of(3)),
-  //       mapCols: sinon.stub().returns(from([1, 1, 1, 1, 1]))
-  //     },
-  //     leafId: 'foobar',
-  //     numSamples: 5,
-  //     columnIndexes: [0, 1, 2],
-  //     parentId: 'foobar',
-  //     depth: 3,
-  //     gainFunction: sinon.spy(),
-  //     _getSamplesInColumn: sinon.stub().returns(from(sampleIds)),
-  //     _getBestSplits: sinon.stub().returns(of(bestSplit)),
-  //     _makeLeaf: sinon.stub().returns(of(leaf)),
-  //     _getLabelCountForSample: sinon.stub().returns(of({
-  //       0: 3,
-  //       1: 2,
-  //     })),
-  //     _getLeafIndex: sinon.stub().returns(from([0, 1, 2, 3, 4]))
-  //   };
-  //   cartLeaf(params).subscribe(onData, e => console.log('leaf.ERR', e), () => {
-  //     expect(onError.called).to.be.false;
-  //     expect(middleware$.count.callCount).to.equal(2);
-  //     expect(params._getSamplesInColumn.callCount).to.equal(3);
-  //     expect(params._getSamplesInColumn.getCall(1).args[0]).to.have.keys([
-  //       'middleware$',
-  //       'sampleId$',
-  //       'totalLabelCount$',
-  //       'columnIndex',
-  //     ]);
-  //     expect(params._getLabelCountForSample.callCount).to.equal(1);
-  //     expect(params._getLabelCountForSample.getCall(0).args[0]).to.have.keys([
-  //       'middleware$',
-  //       'sampleId$',
-  //     ]);
-  //     expect(params._getBestSplits.calledOnce).to.be.true;
-  //     expect(params._getBestSplits.firstCall.args[0]).to.have.keys([
-  //       'columnStreams',
-  //       'totalLabelCount$',
-  //       'gainFunction',
-  //       'maxConcurrentColumnStreams'
-  //     ]);
-  //     expect(params._getLeafIndex.calledOnce).to.be.true;
-  //     expect(params._makeLeaf.calledOnce).to.be.true;
-  //     expect(params._makeLeaf.firstCall.args[0]).to.deep.include({
-  //       depth: params.depth,
-  //       parentId: params.parentId,
-  //       numSamples: params.numSamples,
-  //     });
-  //     expect(onData.calledOnce).to.be.true;
-  //     expect(onData.firstCall.args[0]).to.deep.include({...leaf});
-  //     done();
-  //   });
-  //   if (onError.called) console.log('ERR', onError.firstCall.args[0]);
-  // });
-
-  it('should correctly find the best splits for each column', marbles(m => {
+  it('cartLeaf should correctly create a leaf', marbles(m => {
     const params = {
-      columnStreams: [from([0, 1, 2, 3]), from([4, 5, 6, 7])],
-      totalLabelCount$: of({0: 5, 1: 3}),
-      gainFunction: sinon.spy(),
-      _bestSplit: sinon.stub().returns(source$ => source$.pipe(
-        mapTo({
-          gain: 0.5,
-          splitValue: 3,
-          labelCounts: {0: 4, 1: 4}
-        })
-      )),
+      depth: 2,
+      leafId: 'fakeleafid',
+      numSamples: 4,
+      totalLabelCounts: {0: 2, 1: 2},
+      childIds: ['fakechildidleft', 'fakechildidright'],
       maxConcurrentColumnStreams: 1,
     };
-    const actual$ = getBestSplits(params);
-    const expected$ = m.cold('(01|)', [{
-      gain: 0.5,
-      splitValue: 3,
-      labelCounts: {0: 4, 1: 4},
-      splitColumn: 0,
-    },
-    // {
-    //   gain: 0.5,
-    //   splitValue: 3,
-    //   labelCounts: {0: 4, 1: 4},
-    //   splitColumn: 1,
-    // }
+    const columns$ = m.cold('--0|', [
+      [
+        from([
+          {index: 0, feature: 1, label: 0},
+          {index: 1, feature: 1, label: 1},
+          {index: 2, feature: 2, label: 0},
+          {index: 3, feature: 2, label: 1},
+        ]),
+        from([
+          {index: 0, feature: 97.5, label: 0},
+          {index: 2, feature: 98.8, label: 0},
+          {index: 1, feature: 100.3, label: 1},
+          {index: 3, feature: 100.6, label: 1},
+        ])
+      ]
     ]);
-    m.expect(actual$).toBeObservable(expected$);
+    const leaf$ = columns$.pipe(cartLeaf(params));
+    const expected$ = m.cold('--0|', [
+      {
+        _id: params.leafId,
+        parentId: params.parentId,
+        numSamples: params.numSamples,
+        depth: params.depth,
+        childIds: params.childIds,
+        splitGain: 0.2,
+        splitValue: 100.3,
+        splitColumnIndex: 1,
+        splitLabelCounts: {1: 2, 0: 2},
+      },
+    ]);
+    m.expect(leaf$).toBeObservable(expected$);
   }));
 
   it('makeLeaf() should correctly generate leaf from input data', marbles(m => {
@@ -129,7 +68,7 @@ describe('cartLeaf operator', () => {
       {gain: 0.5, splitValue: 4, splitColumn: 1},
       {gain: 0.3, splitValue: 5, splitColumn: 2},
     ];
-    const input$ = m.cold('01(2|)', splits);
+    const input$ = m.cold('--0-|', [splits]);
     const params = {
       leafId: 'foobar',
       parentId: 'foobardaddy',
@@ -141,87 +80,18 @@ describe('cartLeaf operator', () => {
       // omit childIds since it is random and harder to test...
       map(leaf => omit(leaf, 'childIds'))
     );
-    const expected$ = m.cold('--(3|)', {
-      3: {
+    const expected$ = m.cold('--0-|', {
+      0: {
         parentId: params.parentId,
         numSamples: params.numSamples,
         depth: params.depth,
         _id: params.leafId,
         splitValue: splits[1].splitValue,
-        splitColumn: splits[1].splitColumn,
+        splitColumnIndex: splits[1].splitColumn,
         splitGain: splits[1].gain,
+        splitLabelCounts: {1: 2, 0: 2},
       },
     });
     m.expect(actual$).toBeObservable(expected$);
   }));
-
-  // it('getSamplesInColumn() should properly stream samples from column', done => {
-  //   const onData = sinon.spy();
-  //   const onError = sinon.spy();
-  //   const sampleIds = [0, 1, 2, 3, 4];
-  //   const samples = sampleIds.map(_id => ({_id, squared: _id * _id, label: 0}));
-  //   const middleware$ = from([{foo: 'bar'}]);
-  //   // middleware$.where = sinon.stub().returns(from(sampleIds));
-  //   middleware$.pipe = sinon.stub().returns(from(samples));
-  //   middleware$.where = sinon.stub();
-  //   const params = {
-  //     middleware$,
-  //     sampleId$: from(sampleIds),
-  //     columnIndex: 3,
-  //   };
-  //   getSamplesInColumn(params).subscribe(onData, onError, () => {
-  //     expect(onError.called).to.be.false;
-  //     expect(params.middleware$.pipe.calledOnce).to.be.true;
-  //     expect(params.middleware$.where.calledOnce).to.be.true;
-  //     expect(params.middleware$.where.firstCall.args[0]).to.deep.include({
-  //       _id: {$in: sampleIds},
-  //     });
-  //     expect(params.middleware$.where.firstCall.args[1]).to.deep.include({
-  //       fields: ['_id', 3, 'label'],
-  //       sort: {3: 1}
-  //     });
-  //     expect(onData.callCount).to.equal(5);
-  //     expect(onData.getCall(4).args[0]).to.deep.equal({
-  //       _id: 4,
-  //       feature: 16,
-  //       label: 0
-  //     });
-  //     done();
-  //   });
-  // });
-
-  // it('getLabelCountForSamples() should properly count labels in column', done => {
-  //   const onData = sinon.spy();
-  //   const onError = sinon.spy();
-  //   const sampleIds = [0, 1, 2, 3, 4];
-  //   let middleware$ = from([{foo: 'bar'}]);
-  //   middleware$.pipe = sinon.stub().returns(of(5))
-  //   middleware$.pipe.onCall(0).returns(of(3));
-  //   middleware$.count = sinon.stub()
-  //     .onCall(0).returns(from([3]))
-  //     .returns(from([5]));
-  //   const params = {
-  //     middleware$,
-  //     sampleId$: from(sampleIds),
-  //   };
-  //   const labelCounter$ = getLabelCountForSamples(params);
-  //   labelCounter$.subscribe(onData, onError, () => {
-  //     expect(onError.called).to.be.false;
-  //     expect(middleware$.pipe.callCount).to.equal(2);
-  //     expect(middleware$.count.callCount).to.equal(2);
-  //     expect(middleware$.count.getCall(0).args[0]).to.deep.equal({
-  //       _id: {$in: sampleIds},
-  //       label: {$eq: 1},
-  //     });
-  //     expect(middleware$.count.getCall(1).args[0]).to.be.a('undefined');
-  //     expect(onData.calledOnce).to.be.true;
-  //     expect(onData.firstCall.args[0]).to.deep.equal({0: 2, 1: 3});
-  //     done();
-  //   });
-  //   if (onError.called) console.log('ERR', onError.firstCall.args[0]);
-  // });
-
-  //  it('getLeafIndex() should have a test', done => {
-  //   expect(false);
-  //  });
 });
