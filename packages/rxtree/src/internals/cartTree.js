dc12f77a4d7af3b870a56f85056b1a541e0d520c
Brian <brianflaherty64@gmail.com>
2020-01-30 11:43:11 -0500

    refactor: started to build out cartTree builder

--- packages/rxtree/src/internals/cartTree.js
+++ packages/rxtree/src/internals/cartTree.js
@@ -1,19 +1,61 @@
-import giniGain from './giniGain';
+import {Subject} from 'rxjs';
+import {
+  count,
+  filter,
+  mergeMap,
+  scan,
+  shareReplay,
+  takeUntil,
+  tap
+} from 'rxjs/operators';
+
+import cartLeaf from './cartLeaf';
 import createSortedColumnsFromRows from './createSortedColumnsFromRows';
+import giniGain from './giniGain';
+
+function initializeChildLeaves({parentLeaf}) {
+
+}
 
 const cartTree = function cartTree({
   initialState = {},
   presorted = false,
   gainOperator = giniGain,
   maxLeaves = 100,
-  maxDepth = 10,
+  maxDepth = 9,
 }) {
-  return row$ => row$.pipe(
-    // create a dataframe with all of the data stored as a matrix
-    createSortedColumnsFromRows()
-    // scan((columns, newCols) => (
-    // ), [])
-  );
+  return row$ => {
+    const leafToTrain$ = new Subject();
+    const columns$ = row$.pipe(
+      // create a dataframe with all of the data stored as a matrix
+      createSortedColumnsFromRows(),
+      shareReplay(1)
+    );
+    const leaf$ = leafToTrain$.pipe(
+      mergeMap(leafToTrain => columns$.pipe(
+        filter(), // FIXME - must filter samples correctly
+        // {depth, leafId, numSamples, parentId, totalLabelCounts, childIds
+        cartLeaf({...leafToTrain, gainOperator})
+      )),
+      // stop when maximum leaf condition is hit
+      takeUntil(leafToTrain$.pipe(
+        count(),
+        filter(num => num >= maxLeaves),
+      )),
+      tap(leaf => (
+        leaf.depth < maxDepth
+        ? initializeChildLeaves({parentLeaf: leaf}).map(leafToTrain$.next)
+        : null
+      ))
+    );
+    const tree$ = leaf$.pipe(
+      scan((tree, leaf) => ({
+        ...tree,
+        leaves: [...tree.leaves, leaf],
+      }), {maxDepth, maxLeaves, leaves: []})
+    );
+    return tree$;
+  };
 };
 
 export const testExports = {
