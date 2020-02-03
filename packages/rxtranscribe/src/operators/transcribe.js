58f02b927f8ef654583b4fa86539582231d977de
Brian <brianflaherty64@gmail.com>
2020-02-03 18:47:00 -0500

    feat: rxtranscribe now receives responses from AWS

--- packages/rxtranscribe/src/operators/transcribe.js
+++ packages/rxtranscribe/src/operators/transcribe.js
@@ -4,11 +4,12 @@
 // https://docs.aws.amazon.com/transcribe/latest/dg/streaming.html
 // https://docs.aws.amazon.com/transcribe/latest/dg/limits-guidelines.html
 import { of, throwError } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { map, mergeMap, takeUntil, tap } from 'rxjs/operators';
 import { conduit } from '@bottlenose/rxws';
 
 import createAwsSignedUrl from '../internals/createAwsSignedUrl';
 import convertAudioToBinaryMessage from '../internals/convertAudioToBinaryMessage';
+import decodeMessage from '../internals/decodeMessage';
 
 const transcribe = function transcribe({
   region = (process.env.AWS_REGION || 'us-east-1'),
@@ -16,16 +17,26 @@ const transcribe = function transcribe({
   secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY,
   stop$ = of(),
   _conduit = conduit,
-  _convertAudioToBinaryMessage = convertAudioToBinaryMessage,
+  _serializer = audioBinary => convertAudioToBinaryMessage({audioBinary}),
+  _deserializer = message => decodeMessage({message}),
   _getPresignedUrl = createAwsSignedUrl
 }) {
-  if (!accessKeyId || !secretAccessKey) {
-    return throwError(new Error('AWS credentials must be set'));
-  }
   const url = _getPresignedUrl({region, accessKeyId, secretAccessKey});
   return source$ => source$.pipe(
-    map(audioBinary => _convertAudioToBinaryMessage(audioBinary)),
-    _conduit({url}), // outputs JSON response objects
+    mergeMap(fileChunk => (
+      !accessKeyId || !secretAccessKey
+      ? throwError(new Error('AWS credentials must be set'))
+      : of(fileChunk)
+    )),
+    // map(audioBinary => _convertAudioToBinaryMessage(audioBinary)),
+    // audioBinary should be streamed as an arraybuffer type on the websocket...
+    // tap(d => console.log('IN', d)),
+    _conduit({
+      url,
+      serializer: _serializer,
+      deserializer: _deserializer,
+    }), // outputs JSON response objects
+    tap(out => console.log('OUTPUT', out)),
     takeUntil(stop$)
   );
 };
