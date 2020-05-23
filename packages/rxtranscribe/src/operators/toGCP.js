c1090b5c893743a61255ab08c2e857e41b0d7d66
Cory VanHooser <cvhooser@gmail.com>
2020-05-22 22:24:04 -0400

    Bug fix and word confidence enabled
    Switched to the v1beta to enable word level confidence scores
    
    Fixed a bug with buffers not being concated which was causing accuracy to be static regardless of the stream size send to to the gcp stt client.
    
    Removed excess code

--- packages/rxtranscribe/src/operators/toGCP.js
+++ packages/rxtranscribe/src/operators/toGCP.js
@@ -1,62 +1,45 @@
-import { of, from, throwError, combineLatest, forkJoin, zip} from 'rxjs';
-import { mergeMap, concatMap, takeUntil, map, tap, merge, switchMap, bufferCount, bufferTime, mergeAll, concatAll} from 'rxjs/operators';
+import { of, throwError} from 'rxjs';
+import {concatMap, takeUntil, map, tap, bufferCount, bufferTime, concatAll, count} from 'rxjs/operators';
 import gcpSpeech from '@google-cloud/speech';
 // import bufferBetweenSilences from '../operators/bufferBetweenSilences';
 
+/**
+ *
+ * @param googleCreds credentials to access google cloud
+ * @param stop$ Observable to stop the subscription
+ * @param sampleRate sample rate of of the audio file
+ * @param bufferThreshold the amount of emitted event that will be buffered into a single
+ * streamed request.
+ */
 const toGCP = function toGCP({
   googleCreds = process.env.GOOGLE_APPLICATION_CREDENTIALS,
   stop$ = of(),
-  sampleRate
+  sampleRate = 16000,
+  bufferThreshold = 2
 }) {
   return fileChunk$ => {
+    if (!googleCreds) return throwError(new Error('Google Application Credentials must be set'));
     // Creates a client
-    const clientv1 = new gcpSpeech.SpeechClient();
-    // const clientv1p1beta1 = new gcpSpeech.v1p1beta1.SpeechClient();
+    // const clientv1 = new gcpSpeech.SpeechClient();
+    const clientv1p1beta1 = new gcpSpeech.v1p1beta1.SpeechClient();
     const config = {
       encoding: 'LINEAR16',
       sampleRateHertz: sampleRate,
       languageCode: 'en-US',
+      enableWordConfidence: true,
       // audioChannelCount: 2,
       // enableSeparateRecognitionPerChannel: true,
     };
     return fileChunk$.pipe(
-      mergeMap(fileChunk => (
-        !googleCreds
-        ? throwError(new Error('Google Application Credentials must be set'))
-        : of(fileChunk)
-      )),
-      bufferCount(3),
-      // bufferBetweenSilences({timeInterval: 1000, vadOptions: { sampleRate }}),
-      mergeAll(),
-      // THIS SHOULD WORK!!! WHY AM I GETTING EMPTY RESULTS.......... - Cory
-      // There is no reason why this should be the output should be:
-      //    [[{"results":[]},null,null],[{"results":[]},null,null]]
-      // This should be returning the chunk that has the higher confidence rating.
-      // mergeMap(fileChunk => {
-      //   const audioParams = {config, audio: { content: fileChunk.toString('base64') }};
-      //   return forkJoin([
-      //     clientv1.recognize(audioParams),
-      //     clientv1p1beta1.recognize(audioParams)
-      //   ]).pipe(
-      //     // tap(r => console.log(JSON.stringify(r))),
-      //     map(
-      //       ([one, two]) => (
-      //         one.result.alternatives[0].confidence > two.result.alternatives[0].confidence
-      //         ? one
-      //         : two
-      //       )
-      //     )
-      //   );
-      // }),
-      map(fileChunk => clientv1.recognize({config, audio: { content: fileChunk.toString('base64') }})),
+      bufferCount(bufferThreshold),
+      map(fileChunk => clientv1p1beta1.recognize({config, audio: { content: Buffer.concat(fileChunk).toString('base64') }})),
       concatMap(r => r),
       // tap(r => console.log(JSON.stringify(r))),
       // tap(([res]) => (
       //   console.log(res.results.map(result => result.alternatives[0].confidence).join('\n')))
       // ),
-      // map(([res]) => res.results.map(result => result.alternatives[0].transcript).join(' ')),
+      // map(([res]) => res.results.map(result => result.alternatives[0].transcript).join('')),
       map(([res]) => res.results.map(result => result.alternatives[0].transcript).join('\n')),
-      // tap(r => console.log(JSON.stringify(r))),
       takeUntil(stop$)
     );
   };
