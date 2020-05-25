7c7159fa27634a81ca90e06333756343a062b78a
Brian <brianflaherty64@gmail.com>
2020-05-25 11:30:11 -0400

    refactor: improved unit test coverage for io and fixed issue where it did not call connect

--- packages/rxsocketio/src/creators/io.js
+++ packages/rxsocketio/src/creators/io.js
@@ -1,5 +1,6 @@
+import toPairs from 'lodash/toPairs';
 import socketIO from 'socket.io-client';
-import {Observable,of} from 'rxjs';
+import {Observable,of,throwError} from 'rxjs';
 import {map,shareReplay,takeUntil,tap} from 'rxjs/operators';
 
 import * as actions from '../internals/actions';
@@ -16,55 +17,68 @@ const defaultOptions = {
   upgrade: true,
 };
 
-// how to handle Auth? ideally, like this: {query: {token}}, which does this: http://localhost?token=abc
-// how to handle WSS?
+const eventHandlerMap = {
+  connect: () => actions.connect(),
+  connect_error: error => actions.connectError({error}),
+  connect_timeout: timeout => actions.connectTimeout({timeout}),
+  disconnect: reason => actions.disconnect({reason}),
+  reconnect: attemptNumber => actions.reconnect({attemptNumber}),
+  reconnecting: attemptNumber => actions.reconnecting({attemptNumber}),
+  reconnect_error: error => actions.reconnectError({error}),
+  reconnect_failed: attemptNumber => actions.reconnectFailed({attemptNumber}),
+  ping: () => actions.ping(),
+  pong: latency => actions.pong({latency}),
+};
+
+function socketEventReducer(client, obs) {
+  return (acc, [eventName, handler]) => (
+    client.on(eventName, (...args) => obs.next(handler(...args)))
+  );
+}
+
+function createTopicEmitter(obs, _actions = actions) {
+  return topic => message => obs.next(_actions.newMessage({topic, message}));
+}
+
+function handleTopic(client, obs, emitMessage) {
+  return topic => client.on(topic, emitMessage(topic));
+}
+
+function createObservableFromSocket(
+  client,
+  topics,
+  _actions = actions,
+  _eventHandlerMap = eventHandlerMap,
+) {
+  const topicBlacklist = Object.keys(_eventHandlerMap);
+  if (topicBlacklist.find(key => topics.indexOf(key) > -1)) {
+    return throwError(new Error(
+      `These topics are reserved and cannot be subscribed to: ${topicBlacklist}`
+    ));
+  }
+  return new Observable(obs => {
+    obs.next(_actions.createClient({client}));
+    // map Socket.io events to Observable events
+    toPairs(_eventHandlerMap).reduce(socketEventReducer(client, obs), undefined);
+    // listen for the topics that the client has subscribed to
+    const emitMessageToObs = createTopicEmitter(obs);
+    topics.map(handleTopic(client, obs, emitMessageToObs));
+    client.on('error', error => obs.error(error));
+  });
+}
+
+// Create an observable from a Socket.io client:
 const io = function io({
   url,
   topics = ['message'],
   socketOptions = {},
   stop$ = of(),
   _socketIO = socketIO,
-  _actions = actions
+  _createObservableFromSocket = createObservableFromSocket,
 }) {
-  const ioOptions = {...defaultOptions, socketOptions};
-  const client = _socketIO(url, ioOptions);
-  const action$ = new Observable(obs => {
-    obs.next([client, _actions.createClient({client})]);
-    client.on('connect', () => obs.next(_actions.connect()));
-    client.on('connect_error', error => obs.next(
-      _actions.connectError({error})
-    ));
-    client.on('connect_timeout', timeout => obs.next(
-      _actions.connectTimeout({timeout})
-    ));
-    client.on('disconnect', reason => obs.next(
-      _actions.disconnect({reason})
-    ));
-    client.on('reconnect', attemptNumber => obs.next(
-      _actions.reconnect({attemptNumber})
-    ));
-    client.on('reconnecting', attemptNumber => obs.next(
-      _actions.reconnecting({attemptNumber})
-    ));
-    client.on('reconnect_error', error => obs.next(
-      _actions.reconnectError({error})
-    ));
-    client.on('reconnect_failed', attemptNumber => obs.next(
-      _actions.reconnectFailed({attemptNumber})
-    ));
-    client.on('ping', () => obs.next(
-      _actions.ping(),
-    ));
-    client.on('pong', latency => obs.next(
-      _actions.pong({latency}),
-    ));
-    topics.map(topic => (
-      client.on(topic, message => obs.next(
-        _actions.newMessage({topic, message})
-      ))
-    ));
-    client.on('error', error => obs.error(error));
-  });
+  if (!url) return throwError(new Error('io creator requires a url<String>'));
+  const client = _socketIO.connect(url, {...defaultOptions, ...socketOptions});
+  const action$ = _createObservableFromSocket(client, topics);
   return action$.pipe(
     map(action => [client, action]),
     takeUntil(stop$.pipe(
@@ -74,4 +88,10 @@ const io = function io({
   );
 };
 
+export const testExports = {
+  createObservableFromSocket,
+  createTopicEmitter,
+  handleTopic,
+  socketEventReducer
+};
 export default io;
